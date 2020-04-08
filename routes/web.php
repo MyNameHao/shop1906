1a7d68cb1f9ce890976afc9c7b6999a89d1b55a6
zhangsan <zhangsan@localhost.localdomain>
2020-04-08 16:04:29 +0800

    lool

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test','UserController@test');
-Route::get('user/findpwd','UserController@findpwd');
-Route::post('user/findpwd1','UserController@findpwd1');
-Route::get('user/send','UserController@send');
-Route::get('user/verfind','UserController@verfind');
-Route::post('user/verfinds','UserController@verfinds');
-
-
-
-
 Route::get('/login/login','UserController@login');                      //登录视图
 Route::post('/login/login_do','UserController@loginDo');                 //登录视图
