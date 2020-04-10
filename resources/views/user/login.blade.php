9035f6328b900cdef9d1829b8fb1971aaff1a8c2
zhangsan <2133328533@qq.com>
2020-04-10 12:02:16 +0800

    密码

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -32,7 +32,7 @@
         <button type="submit">Login</button>
     </form>
     <p class=" w3l-register-p">Don't have an account?<a href="{{url('/register')}}" class="register"> Register</a></p>
-    <p class=" w3l-register-p">Retrieve password！<a href="{{url('/find_pass')}}" class="register"> FindPass</a></p>
+    <p class=" w3l-register-p">Retrieve password！<a href="{{url('user/findpwd')}}" class="register"> FindPass</a></p>
     <p class=" w3l-register-p">Change Password！<a href="{{url('/change_pass')}}" class="register"> Change Password</a></p>
 </div>
 <footer>
